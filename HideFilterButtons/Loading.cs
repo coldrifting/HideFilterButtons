90c48d312e4f8904f90a72e112d11e171f8634fb
Aiden Van Dyke <aidenavandyke@gmail.com>
2022-12-27 22:38:34 -0700

    Add project files.

---
+++ HideFilterButtons/Loading.cs
@@ -0,0 +1,58 @@
+﻿using ColossalFramework.UI;
+using ICities;
+using System;
+using UnityEngine;
+
+namespace HideFilterButtons_NS
+{
+    public class Loading : LoadingExtensionBase, IUserMod
+    {
+        public string Name => "Hide Filter Buttons";
+        public string Description => "Removes those annoying little buttons in the build panels";
+
+        /// <summary>
+        /// The actions to take when the level has been loaded
+        /// </summary>
+        public override void OnLevelLoaded(LoadMode mode)
+        {
+            HideFilterButtons();
+        }
+
+        /// <summary>
+        /// Seeks and destroys all filter buttons in the UI.
+        /// </summary>
+        public void HideFilterButtons()
+        {
+            try
+            {
+                UITabContainer gtsContainer;
+                UITabContainer tsContainer = GameObject.Find("TSContainer").GetComponent<UITabContainer>();
+
+                if (tsContainer != null)
+                {
+                    foreach (UIComponent toolPanel in tsContainer.components)
+                    {
+                        if (toolPanel is UIPanel)
+                        {
+                            gtsContainer = toolPanel.GetComponentInChildren<UITabContainer>();
+
+                            if (gtsContainer != null)
+                            {
+                                foreach (UIComponent tabPanel in gtsContainer.components)
+                                {
+                                    UIPanel filterPanel = tabPanel.Find("FilterPanel") as UIPanel;
+                                    filterPanel.Hide();
+                                }
+
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.Log("HideFilterButtons: An issue occurred while hiding a filter button: " + ex.Message);
+            }
+        }
+    }
+}
